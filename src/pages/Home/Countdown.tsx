0d009e7dd0b23c5a5a34486c4ad1e79c9a0cea81
Hindie Suputra <hsuputra@gmail.com>
2025-10-01 15:06:09 +1000

    styling

--- src/pages/Home/Countdown.tsx
+++ src/pages/Home/Countdown.tsx
@@ -1,15 +1,45 @@
+import { useEffect, useState } from "react";
+import { ONE_DAY } from "../../constants/date";
+
 export default function Countdown() {
-  const countdownTimer = {
-    year: 0,
-    months: 3,
-    days: 12,
-  };
+  const [countdownTimer, setCountdownTimer] = useState({
+    years: 0,
+    months: 0,
+    days: 0,
+  });
+
+  useEffect(() => {
+    const targetDate = new Date("2025-04-02");
+
+    const calculateTimePassed = () => {
+      const currentDate = new Date();
+      const timeDifference = currentDate.getTime() - targetDate.getTime();
+
+      const days = Math.floor(timeDifference / ONE_DAY);
+      const months = Math.floor(days / 30);
+      const years = Math.floor(months / 12);
+
+      setCountdownTimer({
+        years,
+        months: months % 12,
+        days: days % 30,
+      });
+    };
+
+    calculateTimePassed();
+
+    const interval = setInterval(() => {
+      calculateTimePassed();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="flex gap-6">
       {Object.entries(countdownTimer).map(([key, values]) => (
-        <div className="flex flex-col bg-blue-200 items-center w-24 h-24 justify-center">
-          <div>{values}</div>
+        <div className="flex flex-col bg-blue-200 items-center w-24 h-24 justify-center border-blue-600 border-b-4 rounded-lg">
+          <div className="text-xl text-blue-500 font-bold">{values}</div>
           <div>{key}</div>
         </div>
       ))}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-blue-400 h-screen p-20">
       <div className="bg-blue-500 mx-auto flex flex-col items-center justify-center py-5 gap-3 border rounded-xl">
-        <div className="text-xl">Days we've been together</div>
+        <div className="text-2xl text-blue-700 font-bold">Days we've been together</div>
         <Countdown />
       </div>
     </div>
