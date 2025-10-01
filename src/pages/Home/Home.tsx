31f81c627c88146b4d37c92e64ec4158b9294d1b
Hindie Suputra <hsuputra@gmail.com>
2025-10-01 16:49:33 +1000

    fix padding issue

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import Countdown from "./Countdown";
 
 export default function Home() {
   return (
-    <div className="bg-blue-400 h-screen p-20">
+    <div className="bg-blue-400 h-screen w-screen p-20">
       <div className="bg-blue-500 mx-auto flex flex-col items-center justify-center py-5 gap-3 border rounded-xl">
         <div className="text-2xl text-blue-700 font-bold">Days we've been together</div>
         <Countdown />
