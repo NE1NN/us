143f81ea69639aa7ce1dcf4f5b4a36f75216c779
Hindie Suputra <hsuputra@gmail.com>
2025-10-01 23:30:11 +1000

    add login

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home/Home";
 import Flowers from "./pages/Flowers/Flowers";
+import Login from "./pages/Login/Login";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/us">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />}/>
+        <Route path="/home" element={<Home />} />
         <Route path="/flowers" element={<Flowers />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router";
+import Block from "../../components/Block";
+import { useState } from "react";
+
+export default function Login() {
+  const [password, setPassword] = useState("");
+  const [wrong, setWrong] = useState(false);
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-blue-400 p-10 flex flex-col gap-5 h-screen w-screen items-center justify-center">
+      <Block className="w-96 px-10">
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (password === "pleasedaddy") {
+              navigate("/home");
+            } else {
+              setWrong(true);
+            }
+          }}
+        >
+          <div className="text-2xl text-blue-700 font-bold text-center">
+            What's The Magic Word ;) (without space)
+          </div>
+          <div>
+            <input
+              type="password"
+              value={password}
+              className="bg-blue-200 w-full rounded-md h-8"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {wrong && <div className="text-red-500">Wrong password 😡</div>}
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-400 rounded-lg py-1 text-white cursor-pointer hover:bg-blue-700"
+          >
+            Enter
+          </button>
+        </form>
+      </Block>
+    </div>
+  );
+}
