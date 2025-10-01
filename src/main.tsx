b4756e7679b4ea2f0deb25faf330190fde538d5e
Hindie Suputra <hsuputra@gmail.com>
2025-10-01 16:15:27 +1000

    add flowers

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Home from "./pages/Home/home.tsx";
+import Home from "./pages/Home/Home";
+import Flowers from "./pages/Flowers/Flowers";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/flowers" element={<Flowers />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
