55e50855a24004240a67d502be488e4158c5a99f
Hindie Suputra <hsuputra@gmail.com>
2025-10-01 21:58:36 +1000

    more pics

--- src/components/Modal/SecondPage.tsx
+++ src/components/Modal/SecondPage.tsx
@@ -12,6 +12,18 @@ import ninth from "../../assets/9th.jpg";
 import tenth from "../../assets/10th.jpg";
 import eleventh from "../../assets/11th.jpg";
 import twelveth from "../../assets/12th.jpg";
+import thirteenth from "../../assets/13th.jpg";
+import fourteenth from "../../assets/14th.jpg";
+import fifteenth from "../../assets/15th.jpg";
+import sixteenth from "../../assets/16th.jpg";
+import seventeenth from "../../assets/17th.jpg";
+import eighteenth from "../../assets/18th.jpg";
+import nineteenth from "../../assets/19th.jpg";
+import twentieth from "../../assets/20th.jpg";
+import twentyfirst from "../../assets/21st.jpg";
+import twentysecond from "../../assets/22nd.jpg";
+import twentythird from "../../assets/23rd.jpg";
+import twentyfourth from "../../assets/24th.jpg";
 
 export default function SecondPage() {
   const images = [
@@ -27,13 +39,25 @@ export default function SecondPage() {
     tenth,
     eleventh,
     twelveth,
+    thirteenth,
+    fourteenth,
+    fifteenth,
+    sixteenth,
+    seventeenth,
+    eighteenth,
+    nineteenth,
+    twentieth,
+    twentyfirst,
+    twentysecond,
+    twentythird,
+    twentyfourth,
   ];
 
   return (
     <Block className="w-3xl z-10 p-5">
       <div className="font-bold text-xl">Pictures Of Us </div>
 
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-6">
         {images.map((i) => (
           <img src={i} className="w-full h-32 object-cover" />
         ))}
