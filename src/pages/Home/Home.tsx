e4230c247c694ce4bee8da7e5afcd6a9cc10d990
Hindie Suputra <hsuputra@gmail.com>
2025-10-01 21:07:47 +1000

    close modal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -37,7 +37,10 @@ export default function Home() {
       {/* Modal */}
       {showModal && (
         <div className="w-screen h-screen absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-          <div className="bg-black opacity-50 absolute flex w-screen h-screen justify-center items-center -z-10" />
+          <div
+            className="bg-black opacity-50 absolute flex w-screen h-screen justify-center items-center -z-10"
+            onClick={() => setShowModal(false)}
+          />
 
           <Block className="w-3xl z-10 p-5">
             <div className="mr-auto font-bold text-xl">Dear Sophie,</div>
