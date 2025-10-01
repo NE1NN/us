37da819844767939598de1e4946b92216810add2
Hindie Suputra <hsuputra@gmail.com>
2025-10-01 14:16:13 +1000

    create days we've been tgt

--- src/pages/Home/Countdown.tsx
+++ src/pages/Home/Countdown.tsx
@@ -1,3 +1,18 @@
 export default function Countdown() {
-  return <div>Countdown</div>;
+  const countdownTimer = {
+    year: 0,
+    months: 3,
+    days: 12,
+  };
+
+  return (
+    <div className="flex gap-6">
+      {Object.entries(countdownTimer).map(([key, values]) => (
+        <div className="flex flex-col bg-blue-200 items-center w-24 h-24 justify-center">
+          <div>{values}</div>
+          <div>{key}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,8 @@ import Countdown from "./Countdown";
 export default function Home() {
   return (
     <div className="bg-blue-400 h-screen p-20">
-      <div className="bg-blue-500 mx-auto flex flex-col items-center justify-center">
-        <div>Days we've been together</div>
+      <div className="bg-blue-500 mx-auto flex flex-col items-center justify-center py-5 gap-3 border rounded-xl">
+        <div className="text-xl">Days we've been together</div>
         <Countdown />
       </div>
     </div>
